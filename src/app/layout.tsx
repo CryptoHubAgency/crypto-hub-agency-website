033c3dcedc9b6c9b81941f96f0b2412b151117dc
M.T. Oyelowo <oyelowomayowa@gmail.com>
2024-07-23 22:22:26 +0100

    fix: default darkTheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class">{children}</ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
